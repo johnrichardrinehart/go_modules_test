d74bad6f781380004754646a8420bf9445b23707
John Rinehart <john@tinkermode.com>
2019-09-18 15:31:48 -0700

    inc to v0.0.4

--- mod2/pkg2/print.go
+++ mod2/pkg2/print.go
@@ -3,7 +3,7 @@ package pkg
 import "fmt"
 
 const PKG = "PKG2"
-const VERSION = "0.0.2"
+const VERSION = "0.0.4"
 
 func PackageAndVersion() string {
 	return fmt.Sprintf("Package %s: Version %s", PKG, VERSION)
