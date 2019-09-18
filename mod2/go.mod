864b7898a0eb70629163b1fe4d8b82db6c16cf2a
John Rinehart <john@tinkermode.com>
2019-09-18 14:32:10 -0700

    renamed dir -> mod

---
+++ mod2/go.mod
@@ -0,0 +1,3 @@
+module github.com/johnrichardrinehart/go_modules_test/cmd2
+
+go 1.13
---
+++ mod2/pkg2/print.go
@@ -0,0 +1,10 @@
+package pkg
+
+import "fmt"
+
+const PKG = "PKG2"
+const VERSION = "0.0.1"
+
+func PackageAndVersion() string {
+	return fmt.Sprintf("Package %s: Version %s", PKG, VERSION)
+}
